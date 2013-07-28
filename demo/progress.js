dea0846673c958488bc3b9b4c401edb3cecae31e
ConnorAtherton <c.liam.atherton@gmail.com>
2013-07-28 23:46:10 +0100

    Remove jQuery and turn file into module pattern

--- demo/progress.js
+++ demo/progress.js
@@ -1,188 +1,117 @@
-/*
- *  Project: Progress.js
- *  Description: Progress is a lightweight jquery plugin that allows students to forecast their future grades.
- *  Author: Connor Atherton, Jordan Kirby
- *  License: 
- */
+progress = function() {
 
-// the semi-colon before function invocation is a safety net against concatenated 
-// scripts and other plugins which may not be closed properly.
-;(function ( $, window, undefined ) {
-
-  // window and document are passed through as local variables rather than globals
-  // as this (slightly) quickens the resolution process and can be more efficiently
-  // minified.
-
-  // Create the defaults once
-  var document = window.document,
-      defaults = {
-        url: "connor.json",
-        tagname: "div",
-      };
-
-  /**
-  *
-  * The plugin constructor and the actual progress
-  * class definition.
-  *
-  **/
-  
-  function Progress( element, options ) {
-    this.element = element,
-    this.modules = [],
-    this.id = element.attr('data-user-id').trim(),
-
-    // Use jQuery extend method to extend/override the default options passed in
-    // at runtime.
-    this.options = $.extend( {}, defaults, options) ;
-
-    this._defaults = defaults;
-
-    this.init();
-    this.getData( this, function( mods ) {
-                    // store result from ajax request into a plugin 
-                    // variable
-                    this.modules = mods;
-    }); 
-
-    console.log( this.modules );
-  }
-
-  Progress.prototype.init = function () {
-    // Place initialization logic here
-    // Already have access to the DOM element and the options 
-
-    if ( this.id.trim() === "undefined" || this.id.trim() === "" ) 
-    {
-        throw new Error('A user id cannot be undefined or empty');
-    }
- 
+  var progress = {
+    version: "0.0.1",
+    data: "Haven't received any data yet"
   };
 
-  Progress.prototype.getData = function(progress, callback) {
-    // ajax request to fetch the data
-    $.ajax({
+  var el;
 
-      async: false,
-      type: "POST",
-      dataType: "json",
-      url: this.options.url ,
-      data: { id: this.id },
+  // public functions
+  progress.init = function(element, url) {
 
-      success: function( json ){
+    var id;
 
-        // If its a success instantiate each module object 
-        modules = progress.extractModules(progress, json);
-        // hand the modules array to callback to store
-        callback( modules );
+    // error checking
+    if( arguments.length === 0 ) throw new TypeError('Function expects two parameters, none given.');
+    typeof element === "string" ? el = document.getElementById( element ) : el = element;
+    // error check the url
 
-      },
+    id = el.getAttribute('data-user-id');
+    if ( id.trim() === "undefined" || id.trim() === "" ) throw new Error('A user id cannot be undefined or empty');
 
-      fail: function() {
+    // get data from url 
+    getData( url );
 
-        // DO SOMETHING BETTER THAN JUST ALERTING!!
-        // Check the cause of the error and give the user some feedback.
-        alert('fail');
+   }
 
-      }, 
+  progress.pie = {
+
+    show: function() {
+      progress.svg  = d3.select( el ).append('svg').attr({ 'width': 960});
+
+      var circles = progress.svg.append('g').selectAll('circle').data(progress.data).enter()
+      .append('circle');
+      circles.attr("cx", function(d, i) {
+        return ( i * 50 ) + 130 ;
+      })
+      .attr("cy", 100)
+      .attr("r", function(d) {
+        return d.overallMark / 4;
+      });
+    },
+
+    hide : function() {
+
+    }
+
+  }
+
+  // private functions ( internal use only )
+  function getData(url) {
+    
+    d3.json( url , function(err, json) {
+
+      if( err ) return console.log(err.message);
+      console.log(json);
+      progress.data = json;
+
+      //
+
+      arrayify(json);
+
+      //
 
     });
 
   }
 
-  Progress.prototype.extractModules = function(progress, json) {
-    var count = 0;
-
-    // loop through json and instantiate each
-    // module and store in the modules array
-    $.each(json.modules, function(i, item) {
-
-        progress.modules[count] = new Module( i , item );
-        count++;
-
-    });
-
-  }
+  function arrayify(json) {
 
 
-  /**
-  *
-  * Individual Module class definition
-  *
-  **/
-  
-  function Module(name, assessments) {
+    var tmpNames = [], tmpMarks = [], tmpWeights = [];
 
-    // Public attributes
-    this.name = name,
-    this.work = assessments,
-    this.overallMark;
+    json.forEach(function(value, index, array) {
 
-    // Arrays that will hold all the broken down data
-    this.workNames = [], this.marks = [], this.weights = [];
+        tmpNames = [], tmpMarks = [], tmpWeights = [];
 
-    // Finally extract all the data and populate the three arrays
-    // created above. 
-    this.extract();
-    this.calcOverall();
+        for(workName in value.work)
+        {
+          // push assessment names to tmp array
+          tmpNames.push(workName);
 
-    }
+          for(key in value.work[workName])
+          {
+            if( key == "mark") 
+              tmpMarks.push(value.work[workName][key]);
+            else 
+              tmpWeights.push(value.work[workName][key]);
 
-  Module.prototype.extract = function() {
-
-    // Get list of the modules associated work
-    for (var key in this.work){
-
-      if(key !== "overallWeight"){
-
-        // Only add module name and not overall weight
-        this.workNames.push(key);
-                
-      }
-
-      // Get individual marks and weights for modules and store in relevant arrays
-      // that were created when the module was instantiated.
-      for(var key2 in this.work[key]){
-                    
-        // key is a weight
-        if(key2 == "weight"){
-                        
-          this.weights.push(this.work[key][key2]);
-        
-        }
-        // key is a mark
-        else{
-
-          // Check that mark isn't undefined. May want to do something a bit more
-          // sophisticated than just saying it is not completed but it will do for
-          // now.
-          if(this.work[key][key2] == "undefined"){
-                            
-            this.marks.push("not completed");
-                        
-          }
-          else{
-
-            this.marks.push(this.work[key][key2]);
-                    
           }
         }
-      }
-    }
+
+        // overide what the work object was
+        value.work = {},
+        // assign new arrays
+        value.work.names = tmpNames,
+        value.work.weights = tmpWeights,
+        value.work.marks = tmpMarks;
+
+    }, this);
+
+
   }
 
-  Module.prototype.calcOverall = function() {
-          
-    this.overall = "overall mark";
+  function objectExtract(obj, callback) {
 
+    for (i in obj) {
+            value = callback.call(obj[i], i, obj[i]);
+
+            if (value === false) break;
+
+        }
   }
 
+  return progress;
 
-  // A really lightweight plugin wrapper around the constructor, 
-  // preventing against multiple instantiations
-  $.fn.progress = function ( options ) {
-    new Progress( this, options );
-    return this;
-  }
-
-}(jQuery, window));
+}();
