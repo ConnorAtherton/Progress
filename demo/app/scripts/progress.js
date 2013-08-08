e85665712cf1ef0396ccc0066040ce67727199c7
ConnorAtherton <c.liam.atherton@gmail.com>
2013-08-08 11:09:15 +0100

    Change the method that shows module names to use pure DOM functions instead of using d3

--- demo/app/scripts/progress.js
+++ demo/app/scripts/progress.js
@@ -7,21 +7,35 @@ progress = function() {
 
   var el;
 
-  // public functions
+  /**
+  *
+  * Progress constructor, takes an element you pass it and stores it as a global plugin
+  * variable used as a container for all the visualisations. Doesn't actually displays the data
+  * but calls getData() which does that.
+  *
+  **/
+  
   progress.init = function(element, url) {
-
-    var id;
 
     // error checking
     if( arguments.length === 0 ) throw new TypeError('Function expects two parameters, none given.');
-    typeof element === "string" ? el = document.getElementById( element ) : el = element;
-    // error check the url
+    // error check for arguments of length 1
 
-    id = el.getAttribute('data-user-id');
+    typeof element === "string" ? el = document.getElementById( element ) : el = element;
+    
+    /**
+    
+      TODO:
+      - USE RegExp to error check the url (maybe?)
+    
+    **/
+    
+    // store it, maybe use it for later 
+    var id = el.getAttribute('data-user-id');
     if ( id.trim() === "undefined" || id.trim() === "" ) throw new Error('A user id cannot be undefined or empty');
 
     // get data from url 
-    getData( url );
+    getData( url, id );
 
   }
 
@@ -32,33 +46,58 @@ progress = function() {
       width : 750,
       height : 150,
       outerRadius : 75,
-      innerRadius: 63,
+      innerRadius: 70,
       pieEl: document.createElement('div'),
       pieId: 'pieCharts',
       paths: null,
       svg: null,
-      startValue: 3
+      text: null,
+      startValue: {startAngle: 6.28, endAngle: 0}
     },
 
+    /**
+    *
+    * Creates a new DOM element used to change between
+    * the two pie states and appends it to the page. 
+    *
+    **/
+    
     show: function() {
-        // only draw the dom elements once
+
+        // If the pie chart is already drawn don't redraw
         if(progress.pie.vars.drawn) return;
  
-        // add on click listener to button to transition
+        /**
+        *
+        * Create a new DOM element and give it a some custom
+        * attributes so it can be targeted within the script and
+        * in CSS.
+        *
+        **/
+        
         var clickTarget = document.createElement('div');
         clickTarget.setAttribute('id', 'pieClick');
         clickTarget.setAttribute('data-update', 'weights');
-        clickTarget.innerHTML = "Click me!";
+        clickTarget.innerHTML = "Show Weights";
         progress.pie.vars.pieEl.appendChild( clickTarget );
 
+        /**
+        *
+        * Add a click event listener and based on the clicktarget's 
+        * 'data-update' attribute choose to update the weights or the
+        * marks.
+        *
+        **/
+        
         d3.select(clickTarget).on("click", function() {
           this.getAttribute("data-update") === "weights" ? progress.pie.updateWeights(this) : progress.pie.updateMarks(this);
         });
 
-        // create new dom element and add to element
+        // Append created DOM element to the element used for the plugin.
         progress.pie.vars.pieEl.setAttribute('id', progress.pie.vars.pieId);
         el.appendChild( progress.pie.vars.pieEl );
 
+        // Show the initial state of the pie charts and add the module names.
         progress.pie.showOverallMarks();
         progress.pie.showModuleNames();
 
@@ -74,17 +113,20 @@ progress = function() {
       var arc = d3.svg.arc()
                   .innerRadius(progress.pie.vars.innerRadius)
                   .outerRadius(progress.pie.vars.outerRadius);
-
-        console.log(b)
-         b.innerRadius = 0;
-         var startValue = {startAngle: 6.28, endAngle: 0};
-         var i = d3.interpolate((this._current || startValue),  b);
+         
+         var i = d3.interpolate((this._current || progress.pie.vars.startValue),  b);
          this._current = i(0);
          return function(t) {
            return arc(i(t));
       };
     },
 
+    /**
+    *
+    * Block comment
+    *
+    **/
+    
     updateWeights: function(that) {
 
       var color = d3.scale.category20c();
@@ -108,8 +150,7 @@ progress = function() {
       progress.pie.vars.paths.transition()
           .ease("sin")
            .duration(250)
-           .attrTween("d", progress.pie.tweenPie)
-           // .each(function(d) { this._current = d; }); // store the angles;
+           .attrTween("d", progress.pie.tweenPie);
 
     },
 
@@ -161,8 +202,8 @@ progress = function() {
               .data(pieData)
               .enter()
               .append("svg")
-                .attr("width", 150)
-                .attr("height", 150);
+                .attr("width", progress.pie.vars.height)
+                .attr("height", progress.pie.vars.height);
 
       progress.pie.vars.paths = progress.pie.vars.svg.selectAll("path")
         .data(function(d, i){ return pie(d) })
@@ -171,10 +212,10 @@ progress = function() {
           .attr("transform", "translate(" + 75 + ", " + 75 + ")");
 
       progress.pie.vars.paths.transition()
-          .ease("sin")
+          .ease("linear")
            .duration(5000)
            .attrTween("d", progress.pie.tweenPie)
-           .each(function(d) { this._current = d; }); // store the angles;
+           .each(function(d) { this._current = d; }); // store the initial angles
 
     },
 
@@ -183,24 +224,66 @@ progress = function() {
       // collect names of modules in array
       var moduleNames = [];
       progress.data.forEach(function(value, index, array) {
-        moduleNames.push(value.name);
+        var tmpArray = [value.name];
+        moduleNames.push(tmpArray);
       });
 
-      var svg = d3.select( progress.pie.vars.pieEl ).selectAll("svg");
+      var svgns = "http://www.w3.org/2000/svg"; // SVG namespace
 
-      var text = svg.selectAll('text')
-          .data(moduleNames)
-          .enter()
-            .append('text')
-            .attr("class", "moduleName")
-            .attr("transform", function(d, i) {
-              return "translate(" + (( progress.pie.vars.width * i ) + progress.pie.vars.outerRadius) + ", " + progress.pie.vars.outerRadius + ")"
-          })
-          .attr('text-anchor', "middle")
-          .attr('alignment-baseline', "middle")
-          .text(function(d, i) {
-            return d;
-          })
+      var textNode;
+
+      var svgs = progress.pie.vars.pieEl.getElementsByTagName('svg');
+        // textNode = document.createTextNode("value");
+        // textEl.appendChild(textNode);
+
+        // var svgArray = toArray(svgs);
+
+        // svgArray.forEach( function(value, index, array) {
+        //     console.log(value)
+        //     value.appendChild(textEl)
+        // }, this);
+
+
+      var i = 0;
+      while(i < moduleNames.length) {
+
+        var textEl = document.createElementNS(svgns, 'text'),
+        textNode = document.createTextNode( moduleNames[i] );
+        textEl.appendChild(textNode);
+
+        textEl.setAttribute("transform", "translate(" + 75 + ", " + 75 + ")");
+        textEl.setAttribute('text-anchor', "middle");
+        textEl.setAttribute('alignment-baseline', "middle");
+        textEl.classList.add("moduleName");
+      
+        svgs[i].appendChild(textEl);
+        // var textNode = document.createTextNode( moduleNames[i] );
+        // textEl.appendChild(textNode);
+        // svgs[i].appendChild(textEl);
+        // console.log(i);
+        i++;
+      }
+
+      //svgs[i].appendChild(textEl);
+
+
+      // progress.pie.vars.text = progress.pie.vars.svg.selectAll('text')
+      //     .data(moduleNames);
+
+      // progress.pie.vars.text
+      //   .enter()
+      //       .append('svg:text')
+      //       .attr("class", "moduleName")
+      //       .attr("transform", function(d, i) {
+      //         return "translate(" + 10 * i + ", " + progress.pie.vars.outerRadius + ")"
+      //     })
+      //     .attr('text-anchor', "middle")
+      //     .attr('alignment-baseline', "middle")
+      //     .text(function(d, i) {
+      //       return d;
+      //     })
+      //     <text class="moduleName" transform="translate(0, 75)" 
+      //       text-anchor="middle" alignment-baseline="middle">MATH210</text>
 
     }
 
@@ -230,49 +313,77 @@ progress = function() {
 
   }
 
-  // private functions ( internal use only )
+  /**
+  *
+  * Fetch the module data asynchronously
+  * and if the call is successful modify the data
+  * into the required format (with arrays) and show all
+  * the graphs on the page.
+  *
+  **/
+  
   function getData(url) {
     
     d3.json( url , function(err, json) {
 
+      /**
+      *
+      * If an error occurs from the url
+      * log it to the console
+      *  
+      *  TODO
+      *
+      *  - Append an error message to the page instead of
+      *    just logging it to the console.
+      *
+      **/
+      
       if( err ) return console.log(err.message);
 
       // return json into array form 
       progress.data = arrayify(json);
-      // show pie charts
+
+      // show pie charts on the page
       progress.pie.show();
 
     });
 
   }
 
+  /**
+  *
+  * Block comment
+  *
+  **/
+  
   function arrayify(json) {
 
-    json.forEach( function(value, index, array) {
+    json.forEach( function(module, index, array) {
 
         var tmpNames = [], tmpMarks = [], tmpWeights = [];
 
-        for(workName in value.work)
+        for(workName in module.work)
         {
           // push assessment names to tmp array
           tmpNames.push(workName);
 
-          for(key in value.work[workName])
+          for(key in module.work[workName])
           {
             if( key == "mark") 
-              tmpMarks.push(value.work[workName][key]);
+              tmpMarks.push(module.work[workName][key]);
             else 
-              tmpWeights.push(value.work[workName][key]);
+              tmpWeights.push(module.work[workName][key]);
 
           }
         }
 
         // overide what the work object was
-        value.work = {},
-        // assign new arrays
-        value.work.names = tmpNames,
-        value.work.weights = tmpWeights,
-        value.work.marks = tmpMarks;
+        module.work = {},
+
+        // assign new arrays to object properties
+        module.work.names = tmpNames,
+        module.work.weights = tmpWeights,
+        module.work.marks = tmpMarks;
 
     }, this);
 
@@ -280,17 +391,29 @@ progress = function() {
 
   }
 
-  function populateArray(property) {
-    // collect names of modules in array
-      var tmpArray = [];
-      progress.data.forEach(function(value, index, array) {
-        console.log('value.prop', value.property);
-        tmpArray.push(value.property);
-      });
-      console.log('tmpArray', tmpArray);
-      return tmpArray;
+  function toArray(list) {
+    var i, array = [];
+    for  (i=0; i<list.length;i++) {array[i] = list[i];}
+    return array;
   }
 
+  function populateArray(property) {
+    // create a temporary array
+    var tmpArray = [];
+
+    progress.data.forEach(function(value, index, array) {
+      tmpArray.push(value.property);
+    });
+
+    // return the array  
+    return tmpArray;
+  }
+
+  /**
+  *
+  * Return the progress object
+  *
+  **/
   return progress;
 
 }();
